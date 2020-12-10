50ca342743805df110f177cd95c17fd15d5fe017
kma <km@stinum.ru>
2020-12-10 09:00:19 +0300

    *

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -4,21 +4,11 @@
 namespace App\Controllers;
 
 
-use App\EventHandlers\TestBeforeResponseHandler;
 use Core\AbstractController;
-use Core\Events\BeforeResponseEvent;
 use Core\Response;
 
 class MainController extends AbstractController
 {
-    public function init()
-    {
-        $this->getEventListener()->on(
-            BeforeResponseEvent::class,
-            TestBeforeResponseHandler::class
-        );
-    }
-
     public function index(): Response
     {
         return new Response(
