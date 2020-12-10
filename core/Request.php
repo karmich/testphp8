80afc5504fb1e056fb853ec4c56ed6e8b52f4bf0
kma <km@stinum.ru>
2020-12-10 15:19:17 +0300

    request

--- core/Request.php
+++ core/Request.php
@@ -7,14 +7,18 @@ class Request
     /**
      * Request constructor.
      * @param string $path
+     * @param array $query
+     * @param array $request
      */
     public function __construct(
-        private string $path
+        private string $path,
+        private array $query,
+        private array $request,
     ){}
 
     public static function createFromGlobals(): Request
     {
-        return new self($_SERVER['PATH_INFO'] ?? "/");
+        return new self($_SERVER['PATH_INFO'] ?? "/", $_GET, $_POST);
     }
 
     /**
@@ -23,5 +27,21 @@ class Request
     public function getPath(): string
     {
         return $this->path;
+    }
+
+    /**
+     * @return array
+     */
+    public function getQuery(): array
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequest(): array
+    {
+        return $this->request;
     }
 }
