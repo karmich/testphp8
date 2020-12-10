02a193bd3cc1f2cfb302a56bfc2ee693014eaf10
kma <km@stinum.ru>
2020-12-10 15:21:34 +0300

    request

--- core/Request.php
+++ core/Request.php
@@ -9,16 +9,22 @@ class Request
      * @param string $path
      * @param array $query
      * @param array $request
+     * @param array $cookies
+     * @param array $files
+     * @param array $server
      */
     public function __construct(
         private string $path,
         private array $query,
         private array $request,
+        private array $cookies,
+        private array $files,
+        private array $server,
     ){}
 
     public static function createFromGlobals(): Request
     {
-        return new self($_SERVER['PATH_INFO'] ?? "/", $_GET, $_POST);
+        return new self($_SERVER['PATH_INFO'] ?? "/", $_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
     }
 
     /**
@@ -43,5 +49,37 @@ class Request
     public function getRequest(): array
     {
         return $this->request;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCookie(): array
+    {
+        return $this->cookies;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCookies(): array
+    {
+        return $this->cookies;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFiles(): array
+    {
+        return $this->files;
+    }
+
+    /**
+     * @return array
+     */
+    public function getServer(): array
+    {
+        return $this->server;
     }
 }
